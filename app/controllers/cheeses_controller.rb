1b290c123a4a9bed3f934c5137a4488ef84f0630
tuanlong1202 <tuanlong1202@gmail.com>
2021-08-29 22:17:21 -0400

    Phase 4 custom rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,12 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    #render json: cheese, only:[:id,:name,:price,:is_best_seller]
+    if cheese
+      render json: cheese, except: [:create_at,:update_at], methods:[:summary]
+    else
+      render json: {error:  'Cheese not found'}, status: :not_found
+    end
   end
 
 end
